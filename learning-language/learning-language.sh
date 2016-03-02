62702240dd47d929ddce8a94a4fed139b0ae2ca2
sasha-bichkov <aleksandar.bichkov@gmail.com>
2016-03-02 18:07:10 +0300

    Added script for learning language

---
+++ learning-language/learning-language.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# LearningLanguage
+
+# USAGE
+# crontab -e
+# The message will show up each 5 mintues
+# */5 * * * * ~/learning-language.sh WORD TRANSLATE
+
+readonly WORD=$1
+readonly TRANSLATE=$2
+
+main() {
+  DISPLAY=:0.0 notify-send -i ~/en.png LearningLanguage "«$WORD» — $TRANSLATE"
+}
+
+main
