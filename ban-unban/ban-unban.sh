cd46521aae1a55e1f10b5f569bac38576e00042a
sasha-bichkov <aleksandar.bichkov@gmail.com>
2016-03-02 22:41:45 +0300

    Added ban-unban

---
+++ ban-unban/ban-unban.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+declare -a blacklist=(
+  gmail.com
+  vk.com
+  facebook.com
+)
+
+ban() {
+  for site in ${blacklist[*]}
+  do
+    echo "127.0.0.1 $site" >> /etc/hosts
+  done
+}
+
+unban() {
+  for site in ${blacklist[*]}
+  do
+    sed -i "/127.0.0.1 $site/d" 1.txt 
+  done
+}
+
+main() {
+  local readonly BAN_TIME="08:00:00"
+  local readonly UNBAN_TIME="14:00:00"
+
+  local readonly CURRENT_TIME=$(date +"%T")
+
+  if [[ $CURRENT_TIME > $BAN_TIME ]]; then
+    ban
+  fi
+
+  if [[ $CURRENT_TIME < $UNBAN_TIME ]]; then
+    unban
+  fi
+}
+
+main
